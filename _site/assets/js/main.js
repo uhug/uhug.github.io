32430d780b71f81352cb865e5dca0335d1c13f2b
uhug <uhug123@hanmail.net>
2020-05-02 07:34:32 +0000

    Deploy with Publish in 2020.05.02-16:34:32

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -45,7 +45,7 @@
                         return
                     }
                     var query = $(this).val();
-                    var searchPage = "https://uhug.github.io/docsy-jekyll/search/?q=" + query;
+                    var searchPage = "https://uhug.github.io/search/?q=" + query;
                     document.location = searchPage;
                     return false;
                 });
