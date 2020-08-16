35346f8a9af63d291318bce7e526c391d24bd94e
DokySp <uhug@naver.com>
2020-08-16 15:24:38 +0900

    문제 클릭 시, 새창에서 띄우도록 CI 배포코드 수정

--- tools/makePost.js
+++ tools/makePost.js
@@ -67,7 +67,7 @@ for(var i=0; i<list.length; i++){
 
     // 태그가 있을 경우, 태그 추가
     if(tag != ' ') context += tag
-    context += '\nfeedback: true\n---\n<h1><img src="https://doky.space/assets/icpclev/'+ lev +'.svg" height="37px"> <a href="http://icpc.me/' + fileName + '">' + title + "</a></h1>\n\n" + button + "\n"
+    context += '\nfeedback: true\n---\n<h1><img src="https://doky.space/assets/icpclev/'+ lev +'.svg" height="37px"> <a href="http://icpc.me/' + fileName + '" target="_blank">' + title + "</a></h1>\n\n" + button + "\n"
 
     // 기존 파일의 앞부분을 새로운 내용으로 변경
     file = file.trim().split('\n')
